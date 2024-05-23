39816a3561fcc7e5c20ab4ac2e09807589653c11
Carlos Otávio de Souza <carlosotavio_souza@apple.com>
2024-05-23 15:26:46 -0300

    finishing home page component

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./AppDownload.css";
+import { assets } from "../../assets/assets";
 
 const AppDownload = () => {
   return (
@@ -8,6 +9,10 @@ const AppDownload = () => {
         For Better Experience Download <br />
         Tomato App
       </p>
+      <div className="app-download-platforms">
+        <img src={assets.play_store} alt="" />
+        <img src={assets.app_store} alt="" />
+      </div>
     </div>
   );
 };
