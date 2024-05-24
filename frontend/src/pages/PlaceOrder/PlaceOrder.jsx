6ff4d3efbbeb3b0ecdda2f6612ef651aa3cd658f
Carlos Otávio de Souza <carlosotavio_souza@apple.com>
2024-05-24 18:04:39 -0300

    starting place and order component

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./PlaceOrder.css";
 
 const PlaceOrder = () => {
-  return <div>PlaceOrder</div>;
+  return (
+    <form className="place-order">
+      <div className="place-order-lef"></div>
+      <div className="place-order-right"></div>
+    </form>
+  );
 };
 
 export default PlaceOrder;
