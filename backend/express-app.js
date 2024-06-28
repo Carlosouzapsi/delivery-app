444e513a30ee64457513321ab46300587cfc9f1a
Carlos Otávio de Souza <carlosotavio_souza@apple.com>
2024-06-28 10:41:56 -0300

    adjusting project

--- backend/express-app.js
+++ backend/express-app.js
@@ -4,6 +4,8 @@ const food = require("./api/food");
 const user = require("./api/user");
 const cart = require("./api/cart");
 const order = require("./api/order");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const HandleErrors = require("./utils/error-handler");
 
 module.exports = async (app) => {
@@ -11,6 +13,7 @@ module.exports = async (app) => {
   app.use(express.urlencoded({ extended: true, limit: "1mb" }));
   app.use(cors());
   app.use("/images", express.static("uploads"));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   food(app);
   user(app);
